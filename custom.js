b6b1272be42089988e28f65a5204d780d062f040
andrew-cao <andycao1443@gmail.com>
2021-08-18 16:31:49 +1000

    added php files, updated css and js files

--- custom.js
+++ custom.js
@@ -1,13 +1,43 @@
-function openTab(tabName) {
-	var i;
-	var x = document.getElementByClassName("tab");
-	for (i = 0; i < x.length; i++){
-		x[i].style.dislay = "none";
+// Tabs
+function openTab(event, tabName) {
+	// Declare variables
+	var i, tabcontent, bar_buttons;
+	
+	
+	// Hide all elements with class="tabcontent"
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++){
+		tabcontent[i].style.display = "none";
 	}
-	tablinks = document.getElementsByClassName("tablink");
-	for (i = 0; i < x.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace("red", "");
+	
+	// Remove class "active" from all "bar_button" class elements
+	bar_buttons = document.getElementsByClassName("bar_button")
+	for (i = 0; i < bar_buttons.length; i++) {
+		bar_buttons[i].className = bar_buttons[i].className.replace(" active", "");
 	}
+	
+	// Show current tab, add active class to button that opened the tab
 	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += "red";
+	event.currentTarget.className += " active";
+	
 }
+
+// Open default tab
+function defaultTab(){
+	document.getElementById('defaultOpen').click()
+}
+
+// Upload file button
+function fileUpload(){
+	document.getElementById('upload_button').addEventListener('click', openUploadFile);
+	function openUploadFile() {
+		document.getElementById('upload_file').click();
+	}
+	
+	document.getElementById('upload_file').addEventListener('change', submitForm);
+	function submitForm(){
+		document.getElementById('form_file').submit();
+	}
+}
+
+
---
+++ upload.php
@@ -0,0 +1,41 @@
+<?php
+// Upload file script
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+
+
+// Check file format
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG or GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+
+// Check if there is an error uploading the file
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// Everything is ok, upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+?>
