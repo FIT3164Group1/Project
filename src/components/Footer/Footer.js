66c15a015cd41883c03f86d52df227d8db60d678
Cooper Corbett <coopercorbett@gmail.com>
2021-10-02 12:57:25 +1000

    💄 Add HR element to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,65 +11,70 @@ function Footer(props) {
 
   const { home, about, contact } = props;
   return (
-    <footer className="px-10 pt-10 pb-5 footer footer-center text-base-100 gap-y-2">
-      <div className="grid grid-flow-col gap-4 text-center ">
-        <a className="link link-hover" href={home}>
-          Home
-        </a>
-        <a className="link link-hover" href={about}>
-          About the Project
-        </a>
-        <a className="link link-hover" href={contact}>
-          Contact
-        </a>
-        <a
-          className="link link-hover"
-          target="_blank"
-          href="https://github.com/FIT3164Group1/Project"
-          rel="noreferrer"
-        >
-          Project Repository
-        </a>
+    <>
+      <div className="px-6 pt-10 pb-6">
+        <hr className="border-base-100" />
       </div>
-      <div className="m-4 dropdown">
-        <div tabIndex="0" className="m-1 btn btn-ghost">
-          CHANGE THEME
+      <footer className="px-10 pb-5 footer footer-center text-base-100 gap-y-2">
+        <div className="grid grid-flow-col gap-4 text-center ">
+          <a className="link link-hover" href={home}>
+            Home
+          </a>
+          <a className="link link-hover" href={about}>
+            About the Project
+          </a>
+          <a className="link link-hover" href={contact}>
+            Contact
+          </a>
+          <a
+            className="link link-hover"
+            target="_blank"
+            href="https://github.com/FIT3164Group1/Project"
+            rel="noreferrer"
+          >
+            Project Repository
+          </a>
         </div>
-        <div className="h-32 mt-16 overflow-y-auto shadow-2xl top-px dropdown-content w-52 rounded-b-box text-base-content">
-          <ul className="p-4 menu compact bg-base-100">
-            <li>
-              <a tabIndex="0" data-set-theme="light" data-act-class="active">
-                🌝 light
-              </a>
-            </li>
-            <li>
-              <a tabIndex="0" data-set-theme="dark" data-act-class="active">
-                🌚 dark
-              </a>
-            </li>
-            <li>
-              <a tabIndex="0" data-set-theme="black" data-act-class="active">
-                🏴 black
-              </a>
-            </li>
-          </ul>
+        <div className="m-4 dropdown">
+          <div tabIndex="0" className="m-1 btn btn-ghost">
+            CHANGE THEME
+          </div>
+          <div className="h-32 mt-16 overflow-y-auto shadow-2xl top-px dropdown-content w-52 rounded-b-box text-base-content">
+            <ul className="p-4 menu compact bg-base-100">
+              <li>
+                <a tabIndex="0" data-set-theme="light" data-act-class="active">
+                  🌝 light
+                </a>
+              </li>
+              <li>
+                <a tabIndex="0" data-set-theme="dark" data-act-class="active">
+                  🌚 dark
+                </a>
+              </li>
+              <li>
+                <a tabIndex="0" data-set-theme="black" data-act-class="active">
+                  🏴 black
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div>
-        <p>Copyright © 2021 - All rights reserved by the development team.</p>
-        <a
-          className="text-center link link-hover"
-          href="https://coopercorbett.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Cooper Corbett
-        </a>
-        <p className="text-center">Qile Wan</p>
-        <p className="text-center">Andrew Cao</p>
-        <p className="text-center">Lewis Ha</p>
-      </div>
-    </footer>
+        <div>
+          <p>Copyright © 2021 - All rights reserved by the development team.</p>
+          <a
+            className="text-center link link-hover"
+            href="https://coopercorbett.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Cooper Corbett
+          </a>
+          <p className="text-center">Qile Wan</p>
+          <p className="text-center">Andrew Cao</p>
+          <p className="text-center">Lewis Ha</p>
+        </div>
+      </footer>
+    </>
   );
 }
 
