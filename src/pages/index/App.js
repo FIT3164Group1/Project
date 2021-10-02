8eb2b5e2b2c6a621a5dbbdb094ee25a5ccd086fe
Cooper Corbett <coopercorbett@gmail.com>
2021-10-02 12:58:28 +1000

    ✨ Add info cards to landing page

--- src/pages/index/App.js
+++ src/pages/index/App.js
@@ -12,7 +12,46 @@ function App() {
   return (
     <>
       <Navbar {...props} />
-      <div className="h-screen">Test</div>
+      <div className="card text-base-content">
+        <div className="card-body">
+          <div className="mb-5 text-center card-title">
+            Gastro-intestinal Cancer Classification
+          </div>
+          <div className="flex flex-col gap-3 md:flex-row">
+            <div className="w-full md:w-1/2">
+              <div className="shadow-2xl card bordered">
+                <div className="card-body">
+                  <div className="text-center card-title">
+                    <div className="p-5 text-2xl badge badge-primary">Goal</div>
+                  </div>
+                  <p>
+                    Utilise our cancer prediction model with your own
+                    histological image.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2">
+              <div className="shadow-2xl card bordered">
+                <div className="card-body">
+                  <div className="text-center card-title">
+                    <div className="p-5 text-2xl badge badge-secondary">
+                      Strategy
+                    </div>
+                  </div>
+                  <p>
+                    We create a classfication model to identify between the two
+                    possible categories MSS and MSIMUS (MSI) of cancer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-6">
+          <hr className="border-base-100" />
+        </div>
+      </div>
       <Footer {...props} />
     </>
   );
