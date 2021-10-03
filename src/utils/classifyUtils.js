177506e76f0ff5e21a339819945fda13c8da238b
Cooper Corbett <coopercorbett@gmail.com>
2021-10-03 20:14:25 +1100

    ✨ Generate URL safe Base64 encoded strings from image files and make Axios request to AWS API Gateway endpoint

--- src/utils/classifyUtils.js
+++ src/utils/classifyUtils.js
@@ -1,4 +1,47 @@
-export const classifyImageFiles = (fileList) => {
-  console.log(fileList);
+import { encode, trim } from "url-safe-base64";
+import axios from "axios";
+
+const generateBase64Encoding = async (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = async () => {
+      try {
+        // Resolve the promise with the response value
+        const string = reader.result.split(",")[1];
+        const urlSafeEncoding = trim(encode(string));
+        resolve(urlSafeEncoding);
+      } catch (err) {
+        reject(err);
+      }
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    reader.readAsDataURL(file);
+  });
+};
+
+export const classifyImageFiles = async (fileList) => {
+  const imageArray = await Promise.all(fileList.map(generateBase64Encoding));
+
+  const imageData = {
+    instances: imageArray.map((base64) => [base64]),
+  };
+
+  const config = {
+    url:
+      "https://grx408sfch.execute-api.ap-southeast-2.amazonaws.com/prod/make-classification",
+    method: "post",
+    data: imageData,
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const response = await axios(config);
+  const predictions = response.data.predictions;
+  predictions.map((prediction, index) =>
+    console.log(`Prediction ${index}: ${prediction}`)
+  );
   return;
 };
